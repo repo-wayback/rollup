bc541d0becbd851cbea92cafa93a408388a9f359
XiaoPi <530257315@qq.com>
2024-03-18 14:22:17 +0800

    Simplify the loop of tree-shaking

--- src/Module.ts
+++ src/Module.ts
@@ -218,7 +218,7 @@ export default class Module {
 	readonly dynamicDependencies = new Set<Module | ExternalModule>();
 	readonly dynamicImporters: string[] = [];
 	readonly dynamicImports: DynamicImport[] = [];
-	dynamicDependenciesIncludeAllExports = new Set<Module>();
+	private dynamicDependenciesIncludeAllExports = new Set<Module>();
 	excludeFromSourcemap: boolean;
 	execIndex = Infinity;
 	readonly implicitlyLoadedAfter = new Set<Module>();
@@ -693,6 +693,12 @@ export default class Module {
 
 	hasEffects(): boolean {
 		return this.info.moduleSideEffects === 'no-treeshake' || this.ast!.hasCachedEffects();
+	}
+
+	includeDynamicDependenciesIncludeAllExports() {
+		for (const dynamicDependencies of this.dynamicDependenciesIncludeAllExports) {
+			dynamicDependencies.includeAllExports(true);
+		}
 	}
 
 	include(): void {
